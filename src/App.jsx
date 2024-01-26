3d336184abd58c16d50767184ef53e8ce21d2685
devxshubham <shubhamdevcode@gmail.com>
2024-01-26 23:27:45 +0530

    adding search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,62 @@
 import { useState, useEffect } from 'react'
+import { atomInfo } from '../store/atoms/info'
 
 import './App.css'
 
 function App() {
-  const [info, setInfo] = useState()
+  const [info, setInfo] = useState("")
+  const [change, setChange] = useState(0);
+  const [input, setInput] = useState('')
   // console.log(info)
 
   useEffect( ()=>{
-    fetch('https://api.github.com/users/devxshubham')
+    fetch(`https://api.github.com/users/${input}`)
     .then( async(response) => {
       const  data = await response.json();
       console.log(data)
       setInfo(data)
     })
-  },[])
+  },[change])
 
   return (
     <div className='container'>
-      { info ? (
-          <>
-            <div id='image'>
-              <img src="https://avatars.githubusercontent.com/u/122411505?v=4" alt=""/>
-            </div>
-            <div className='login'>{info.login}</div>
-            <div id='info'>
-              <span>
-                <div>{info.followers}</div>
-                <div className='details'>followers</div>
-              </span>
-              <span>
-                <div>{info.following}</div>
-                <div className='details'>following</div>
-              </span>
-              <span>
-                <div>{info.public_repos}</div>
-                <div className='details'>public repos</div>
-              </span>
-            </div>
-                  
-          </>
-      ) :
-      (
-        <>
-          <div>...loading</div>
-        </>
-      )}
+      <div id='image'>
+        <img src={info.avatar_url} alt=""/>
+      </div>
+      <div className='login'>{info.login}</div>
+      <div id='info'>
+        <span>
+          <div>{info.followers}</div>
+          <div className='details'>followers</div>
+        </span>
+        <span>
+          <div>{info.following}</div>
+          <div className='details'>following</div>
+        </span>
+        <span>
+          <div>{info.public_repos}</div>
+          <div className='details'>public repos</div>
+        </span>
+      </div>
+
+      <InputGit setInput={setInput} setChange={setChange}></InputGit>
+
+      
     </div>
   )
+}
+function InputGit({setInput,setChange}){
+  
+
+  return <div>
+    <input type="text" onChange={(e)=>{
+      setInput(e.target.value);
+    }}/>
+    <button onClick={()=>{
+      setChange( prev => prev+1);
+    }}>search</button>
+  </div>
+    
 }
 
 export default App
